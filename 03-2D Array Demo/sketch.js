9cea34418d62ee1c97f21b0d68e8ec7081baec4b
Raffey0802441 <0802441@sps-student.sk.ca>
2020-09-28 11:48:06 -0600

    working on 2D deno

--- 03-2D Array Demo/sketch.js
+++ 03-2D Array Demo/sketch.js
@@ -5,10 +5,62 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let grid;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  grid = createGird(10);
 }
 
 function draw() {
   background(220);
+
+  displayGrid();
+}
+
+function keyPressed(){
+  if (key === " ") {
+    grid = createGird(10);
+  }
+}
+
+function createGird(gridSize) {
+  let grid = [];
+  for (let i = 0; i < gridSize; i++) {
+    grid.push([]);
+    for (let j = 0; j < gridSize; j++) {
+      if (random(100) < 50) {
+        grid[i].push(0);
+      }
+      else {
+        grid[i].push(1);
+      }
+    }
+  }
+  return grid;
+}
+
+function displayGrid() {
+  let cellwidth = width / grid[0].length;
+  let cellHeight = height / grid.length;
+
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x]=== 0){
+        fill("Black");
+      }
+
+      else{
+        fill("white");
+      }
+
+      rect(cellwidth * x, cellHeight * y, cellwidth, cellHeight);
+    }
+  }
+}
+
+function cellClicked() {
+  let cellwidth = width / grid[0].length;
+  let cellHeight = height / grid.length; 
 }
