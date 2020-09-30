361007eac86c03a5073cc1725fb21b7422b5b6b6
web-cro <58191553+web-cro@users.noreply.github.com>
2020-09-30 10:04:12 -0600

    click to change color

--- 03-2D Array Demo/sketch.js
+++ 03-2D Array Demo/sketch.js
@@ -6,16 +6,35 @@
 // - describe what you did to take this project "above and beyond"
 
 let grid;
+let cellWidth;
+let cellHeight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   grid = createGird(10);
+  cellWidth = width / grid[0].length;
+  cellHeight = height / grid.length;
 }
 
 function draw() {
   background(220);
 
   displayGrid();
+}
+
+function mousePressed() {
+  let cellX = floor(mouseX / cellWidth);
+  let cellY = floor(mouseY / cellHeight);
+
+  console.log(cellX, cellY);
+  
+  if (grid[cellY][cellX] === 0){
+    grid[cellY][cellX] = 1;
+  }
+
+  else {
+    grid[cellY][cellX] = 0;
+  }
 }
 
 function keyPressed(){
@@ -41,10 +60,6 @@ function createGird(gridSize) {
 }
 
 function displayGrid() {
-  let cellwidth = width / grid[0].length;
-  let cellHeight = height / grid.length;
-
-
   for (let y = 0; y < grid.length; y++) {
     for (let x = 0; x < grid[y].length; x++) {
       if (grid[y][x]=== 0){
@@ -55,7 +70,7 @@ function displayGrid() {
         fill("white");
       }
 
-      rect(cellwidth * x, cellHeight * y, cellwidth, cellHeight);
+      rect(cellWidth * x, cellHeight * y, cellWidth, cellHeight);
     }
   }
 }
