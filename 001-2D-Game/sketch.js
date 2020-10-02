cb6a1efaa87ce5fe3fedd16af08b28d494c15369
web-cro <58191553+web-cro@users.noreply.github.com>
2020-10-02 10:20:53 -0600

    saving  the grid layout on a file

--- 001-2D-Game/sketch.js
+++ 001-2D-Game/sketch.js
@@ -8,45 +8,48 @@
 const GRIDSIZE = 15;
 let cellWidth;
 let cellHeight;
+let level;
 
-let grid = 
-        [[1,1,1,1,1,1,1,1,1,1,1,1,1,2,2],
-          [1,1,1,1,1,1,0,0,0,0,0,0,1,2,2],
-          [1,1,1,1,1,1,0,1,1,1,1,0,1,2,2],
-          [1,1,1,1,1,1,0,1,1,1,1,0,1,2,2],
-          [0,0,1,1,1,1,0,1,1,1,1,0,1,2,2],
-          [1,0,0,0,0,1,0,1,1,1,1,0,1,2,2],
-          [1,1,1,1,0,1,0,1,1,1,1,0,1,2,2],
-          [1,1,1,1,0,1,0,1,1,1,1,0,1,2,2],
-          [1,1,1,1,0,1,0,1,1,1,1,0,1,2,2],
-          [1,1,1,1,0,1,0,1,1,1,1,0,1,2,2],
-          [1,1,1,1,0,0,0,1,1,1,1,0,1,2,2],
-          [1,1,1,1,1,1,1,1,1,1,1,0,1,2,2],
-          [1,1,1,1,1,1,1,1,1,1,1,0,1,2,2],
-          [1,1,1,1,1,1,1,1,1,1,1,0,0,2,2],
-          [1,1,1,1,1,1,1,1,1,1,1,1,1,2,2]];
+function preload() {
+  level = loadStrings("assets/1level.txt");
+  level = loadStrings("assets/2level.txt");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  cellWidth = width / grid[0].length;
-  cellHeight = height / grid.length;
+  // convert Level into 2d array
+  for (let i=0; i<level.length; i++) {
+    level[i] = level[i].split(",");
+  }
+
+  //loop through the whole 2d array, and turn everything to numbers
+  for (let y=0; y<GRIDSIZE; y++) {
+    for (let x=0; x<GRIDSIZE; x++) {
+      level[y][x] = int(level[y][x]);
+    }
+  }
+
+
+  cellWidth = width / level[0].length;
+  cellHeight = height / level.length;
 }
 
 function draw() {
   background(220);
 
-  displayGrid();
+  displaylevel();
 }
 
-function displayGrid() {
+function displaylevel() {
   for (let y = 0; y < GRIDSIZE; y++) {
     for (let x = 0; x < GRIDSIZE; x++) {
 
-      if (grid[y][x] === 0) {
+      if (level[y][x] === 0) {
         fill(48,48,48);
       }
 
-      else if (grid[y][x] === 2){
+      else if (level[y][x] === 2){
         fill(61,30,0);
       }
 
