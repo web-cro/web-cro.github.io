eeb85d27b3a5f31f8a43702a82e824ebd32ec030
Raffey0802441 <0802441@sps-student.sk.ca>
2020-10-07 09:06:03 -0600

    pathfinder

---
+++ 0001-pathfinder/sketch.js
@@ -0,0 +1,202 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+// **********************************************************************
+//  A* educated guess to find the best path from point A to point B
+//  A* formula is f(n) = g(n) + h(n)
+//  f(n) = time
+//  g(n) = time from startingPoint to finish
+//  h(n) = IDK ??
+
+const GRIDSIZE = 30;
+let grid = new Array(GRIDSIZE); // How does "New Array" work?
+
+let cellsToCheck = [];
+let cellThatHaveBeenChecked = [];
+let startingPoint;
+let endingPoint;
+let cellWidth, cellHeight;
+let path = [];
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    makeGrid();
+
+    startingPoint = grid[0][0]; // startingPoint point
+    endingPoint = grid[29][0]; // endingPoint point
+
+    cellsToCheck.push(startingPoint);
+
+    console.log(grid);
+}
+
+function draw() {
+    findPath();
+
+    background(0);
+
+    // display grid
+    for (let i = 0; i < GRIDSIZE; i++) {
+        for (let j = 0; j < GRIDSIZE; j++) {
+            grid[i][j].displayGrid(color(255));
+        }
+    }
+    // Display the fastest path from startingPoint to finish
+    for (let i = 0; i < cellThatHaveBeenChecked.length; i++) {
+        cellThatHaveBeenChecked[i].displayGrid(color(255, 0, 0));
+
+    }
+    // change the color of the cells that have already been checked
+    for (let i = 0; i < cellsToCheck.length; i++) {
+        cellsToCheck[i].displayGrid(color(0, 255, 0));
+    }
+
+    for (let i = 0; i < path.length; i++) {
+        path[i].displayGrid(color(0, 0, 255));
+    }
+}
+
+// look through the array and remove a cells that we have already visted
+function removeFromArray(array, value) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (array[i] === value) {
+            array.splice(i, 1);
+        }
+    }
+}
+
+// check the distance between the startingPointing and endingPointing points
+function checkDistance(a , b) {
+    let distance = abs(a.i - b.i) + abs(a.j - b.j);
+    return distance;
+}
+
+class Pathfinder {
+    constructor(i, j) {
+        this.i = i;
+        this.j = j;
+
+        this.f = 0;
+        this.g = 0;
+        this.h = 0;
+
+        this.neighbors = [];
+        this.previous = undefined;
+    }
+    // create and color rects to use when display grid
+    displayGrid(color) {
+        fill(color);
+        rect(this.i * cellWidth, this.j * cellHeight, cellWidth - 1, cellHeight - 1);
+    }
+
+    checkNeighbors(grid) {
+        let i = this.i;
+        let j = this. j;
+        // Check neighbors
+        if (i < GRIDSIZE - 1) {
+            this.neighbors.push(grid[i + 1] [j]);
+        }
+
+        if (i > 0) {
+            this.neighbors.push(grid[i - 1] [j]);
+        }
+
+        if (j < GRIDSIZE - 1) {
+            this.neighbors.push(grid[i] [j + 1]);
+        }
+        
+        if (j > 0) {
+            this.neighbors.push(grid[i] [j - 1]);
+        }
+    }
+}
+
+// A grid
+function makeGrid() {
+    cellWidth = width / GRIDSIZE;
+    cellHeight = height / GRIDSIZE;
+
+    // making an 2D array
+    for (let i = 0; i < GRIDSIZE; i++) {
+        grid[i] = new Array(GRIDSIZE);
+    }
+
+    for (let i = 0; i < GRIDSIZE; i++) {
+        for (let j = 0; j < GRIDSIZE; j++) {
+            grid[i][j] = new Pathfinder (i, j); // is new used much like a let to Define funtions
+        }
+    }
+
+    for (let i = 0; i < GRIDSIZE; i++) {
+        for (let j = 0; j < GRIDSIZE; j++) {
+            grid[i][j].checkNeighbors(grid);
+        }
+    }
+
+    
+}
+
+function findPath () {
+
+    // keep searching for A path
+    if (cellsToCheck.length > 0) { 
+
+        let lowestValue = 0;
+        for (let i = 0; i < cellsToCheck.length; i++) {
+            if (cellsToCheck[i].f < cellsToCheck[lowestValue].f) {
+                lowestValue = i;
+            }
+        }
+        let currentValue = cellsToCheck[lowestValue]; 
+
+        if (currentValue === endingPoint) {
+
+            // find the path
+            path = [];
+            let value = currentValue;
+            while (value.previous) {
+                path.push(value.previous);
+                value = value.previous;
+            }
+            noLoop();
+            console.log("DONE");
+        }
+
+        // remove the value fron the cellsToCheck and push it into the cellThatHaveBeenChecked
+        removeFromArray(cellsToCheck, currentValue);
+        cellThatHaveBeenChecked.push(currentValue);
+
+        let neighbors = currentValue.neighbors;
+        for (let i = 0; i < neighbors.length; i++) {
+            let neighborsToCheck = neighbors[i];
+
+            if (!cellThatHaveBeenChecked.includes(neighborsToCheck)) { // what does include mean
+                let gValue = currentValue.g + 1;
+
+                if (cellsToCheck.includes(neighborsToCheck)) {
+                    if (gValue < neighborsToCheck.g) {
+                        neighborsToCheck.g = gValue;
+                    }
+                }
+                else {
+                    neighborsToCheck.g = gValue;
+                    cellsToCheck.push(neighborsToCheck);
+                }
+                // make an educated guess for the fastest path
+                neighborsToCheck.h = checkDistance(neighborsToCheck, endingPoint);
+                neighborsToCheck.f = neighborsToCheck.g = neighborsToCheck.h;
+                neighborsToCheck.previous = currentValue;
+            }
+        }
+    }
+
+    // No Solution
+    else {
+        console.log("No Solution");
+    }
+}
